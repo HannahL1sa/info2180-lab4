6e4a618cf6bac2bd0e1282627e2ed22e4adf1764
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2021-11-01 22:53:33 -0500

    Look up any Superhero by typing in the name or alias in a Textfield

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
 window.onload = function(){
-  var SearchBtn = document.getElementById("search");
+  var searchBtn = document.getElementById("search");
+  //var name = document.getElementById("name");
+  
 
-  SearchBtn.addEventListener("click", function(event){
+  searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
     var httpRequest = new XMLHttpRequest();
-    var url = "superheroes.php";
-    httpRequest.onreadystatechange = loadAlert;
+    var query = document.querySelector("input");
+    var input = query.value;
+    var valid = false;
+    //var url = "http://localhost/info2180-lab4/superheroes.php?query="+input;
+
+    if (!isValidInput(input.trim())) {
+      valid = false;
+      alert("Incorrect format for text. Only letters and spaces allowed.");
+    }
+
+    function isValidInput(input) {
+      var regex = /^([a-zA-Z\s-'])*$/;
+      //var regex = /^[\w\s-']+$/;
+      if(regex.test(input)){
+        return true
+      }
+    }
+
+    var url = "superheroes.php?query="+input;
+    httpRequest.onreadystatechange = searchForHero;
     httpRequest.open('GET', url);
     httpRequest.send();
-    //event.preventDefault();
-    //console.log("button clicked");
-    //var httpRequest = new XMLHttpRequest();
-    //var url = "superheroes.php";
-    //httpRequest.open('GET', url);
-    //httpRequest.onreadystatechange = loadQuote;
-    //httpRequest.send();
 
-    function loadAlert() {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-          var response = httpRequest.responseText;
-          alert(response);
-        //var quote = document.getElementById("quote");
-        //quote.innerHTML = response;
-        } 
-        else {
-          alert('There was a problem with the request.');
+
+    function searchForHero(){
+      if(isValidInput(input)==true){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            var response = httpRequest.responseText;
+            var result = document.getElementById("result");
+            result.innerHTML = response;
+          } 
+          else {
+            alert('There was a problem with the request.');
         }
+       }
       } 
     }
   });
